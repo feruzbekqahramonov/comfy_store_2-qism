931e0e003fd42567ab53c680718a6c73086333bd
feruzbek715 <nazirjon502@gmail.com>
2024-04-25 22:22:00 +0500

    search qism tugadi

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Card from "../components/Card";
 
 function Products() {
@@ -18,6 +18,21 @@ function Products() {
         setLoading(false)
       })
   }, [])
+
+  const searchRef = useRef()
+
+  function hanldecl (e) {
+    e.preventDefault();
+    let name = searchRef.current.value
+
+      fetch(`https://strapi-store-server.onrender.com/api/products?search=${name}`)
+      .then(res => res.json())
+      .then(data => {
+        setFeatured(data.data)
+      })
+
+    
+  }
   return (
     <div className="w-4/5 mx-auto mt-20">
       <div className="filter p-4 bg-primary-content rounded-md">
@@ -30,6 +45,7 @@ function Products() {
               type="search"
               id="search"
               className="input input-bordered w-full max-w-xs input-sm"
+              ref={searchRef}
             />
           </div>
           <div className="field flex flex-col gap-1 w-1/4">
@@ -93,8 +109,8 @@ function Products() {
             </div>
           </div>
           <div className="buttons w-2/4 flex justify-between gap-3">
-          <button className="btn btn-primary w-2/4">Primary</button>
-            <button className="btn btn-secondary w-2/4">Secondary</button> 
+          <button className="btn btn-primary w-2/4" onClick={hanldecl}>SEARCH</button>
+            <button className="btn btn-secondary w-2/4">RESET</button> 
           </div>
         </div>
       </div>
